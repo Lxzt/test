c2542a2531f969642fe95e6f5ec330dfc1c4eb93
“Lxzt” <2421432149@qq.com>
2023-12-14 17:25:09 +0800

    java测试

---
+++ Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhangyaqiang
+ * time 2023-12-14-17:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("======java======");
+    }
+}
